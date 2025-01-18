9858c504af1994cd01cb3959e6a5bb178a836d22
Metosfer <metehanbozdagg@hotmail.com>
2025-01-19 00:24:21 +0200

    Added Cutscene and fixed some animation bugs

--- TheLostExhibit/Assets/RiverQuest/Rock.cs
+++ TheLostExhibit/Assets/RiverQuest/Rock.cs
@@ -7,22 +7,26 @@ public class Rock : MonoBehaviour
     public float destroyDuration = 0.3f; // Taþýn yok olma süresi
 
 
+
     private void Start()
     {
-       
+        
     }
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Player") && !isStable)
         {
             Fall();
+            
         }
     }
 
     private void Fall()
     {
-        
+
         
         Destroy(gameObject, destroyDuration); // 3 saniye sonra taþý yok et
+        
     }
 }
