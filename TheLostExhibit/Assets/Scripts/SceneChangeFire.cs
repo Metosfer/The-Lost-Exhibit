d64dba4b43f9f1651dcf0c754bfcbd11a547228a
tatisiate <162497241+tatisiate@users.noreply.github.com>
2025-01-26 20:54:17 +0200

    -

---
+++ TheLostExhibit/Assets/Scripts/SceneChangeFire.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChangeFire : MonoBehaviour
+{
+    private bool isPlayerInTrigger = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            isPlayerInTrigger = true;
+            Debug.Log("Player entered trigger");
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            isPlayerInTrigger = false;
+            Debug.Log("Player exited trigger");
+        }
+    }
+
+    private void Update()
+    {
+        if (isPlayerInTrigger && Input.GetKeyDown(KeyCode.E))
+        {
+            Time.timeScale = 0;
+            SceneManager.LoadScene(7);
+
+
+        }
+    }
+}
